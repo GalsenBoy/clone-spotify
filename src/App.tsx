a3a07cbb80bc5da33fa886125ae73873a932d72b
bilaly <billycsk@gmail.com>
2023-08-07 15:04:20 +0200

    Ajout de routes et browserRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import "./App.css";
 import Aside from "./pages/Aside";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <>
-      <Aside />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path={"/aside"} element={<Aside />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- src/components/Explore.tsx
+++ src/components/Explore.tsx
@@ -5,6 +5,7 @@ export default function Explore() {
     <div>
       <Link to={"/home"} content={"Accueil"} />
       <Link to={"/search"} content={"Rechercher"} />
+      <h1>BLALBLA</h1>
     </div>
   );
 }
