d8839eb6f17499d1bf5328478951e0e9afc6f3ce
bilaly <billycsk@gmail.com>
2023-08-10 12:23:28 +0200

    clean code

--- src/components/Explore.tsx
+++ src/components/Explore.tsx
@@ -1,18 +1,32 @@
-// import { Home as HomeIcon } from "lucide-react";
+// import { Home as HomeIcon, icons } from 'lucide-react';
 import Link from "../composable/Links";
 import Icon from "../composable/Icon";
 
 export default function Explore() {
+  const exploreData = [
+    {
+      name: "Home",
+      color: "red",
+      size: 24,
+      to: "/home",
+      content: "Accueil",
+    },
+    {
+      name: "Search",
+      color: "red",
+      size: 24,
+      to: "/search",
+      content: "Rechercher",
+    },
+  ];
   return (
-    <section className="flex flex-col p-2 max-w-xs rounded-md bg-black gap-2 ">
-      <div className="flex gap-5 py-2">
-        <Icon name="Home" color="red" size={24} />
-        <Link to="/home" content="Accueil" />
-      </div>{" "}
-      <div className="flex gap-5 py-2">
-        <Icon name="Search" color="red" size={24} />
-        <Link to="/search" content="Rechercher" />
-      </div>
+    <section className="flex flex-col p-2 max-w-xs rounded-md bg-black gap-2 mb-2">
+      {exploreData.map((item) => (
+        <div className="flex gap-5 py-2">
+          <Icon name={item.name} color={item.color} size={item.size} />
+          <Link to={item.to} content={item.content} />
+        </div>
+      ))}
     </section>
   );
 }
