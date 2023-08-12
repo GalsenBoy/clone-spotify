040610ecc0f73f5a19708b521bc364123f43805d
bilaly <billycsk@gmail.com>
2023-08-12 17:05:15 +0200

    Clean code

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,6 +6,15 @@ export default function Header() {
   const [data, setData] = useState({});
   const clientId = import.meta.env.VITE_SPOTIFY_CLIENT_ID;
 
+  const chevronData = [
+    {
+      name: "ChevronLeft",
+    },
+    {
+      name: "ChevronRight",
+    },
+  ];
+
   useEffect(() => {
     const fetchData = async () => {
       try {
@@ -28,20 +37,15 @@ export default function Header() {
   return (
     <header className="flex justify-between  bg-black p-6 my-2 mr-2 rounded-md w-full">
       <div className="flex gap-2">
-        <div>
+        {chevronData.map((index, item) => (
           <Icon
+            key={item}
             className=" rounded-full bg-blackIcon"
-            name="ChevronLeft"
+            name={index.name}
             size={24}
             color={"red"}
           />
-        </div>
-        <Icon
-          className=" rounded-full bg-blackIcon"
-          name="ChevronRight"
-          size={24}
-          color={"red"}
-        />
+        ))}
       </div>
       <div className="space-x-6">
         <Button content="S'inscrire" />
