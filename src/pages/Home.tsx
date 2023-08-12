929a2e2053e43c5a08e4b570ad8ed278db607738
bilaly <billycsk@gmail.com>
2023-08-12 17:43:07 +0200

    Ajout de Jest + preventDefault sur le clique droit

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,8 +3,15 @@ import Footer from "../components/Footer";
 import Header from "../components/Header";
 
 export default function Home() {
+  const preventContextMenu = (e: React.MouseEvent) => {
+    e.preventDefault();
+  };
   return (
-    <div className="bg-dark font-circular">
+    <div
+      onContextMenu={preventContextMenu}
+      style={{ width: "100%", height: "100%" }}
+      className="bg-dark font-circular"
+    >
       <div className="flex">
         <Aside />
         <Header />
